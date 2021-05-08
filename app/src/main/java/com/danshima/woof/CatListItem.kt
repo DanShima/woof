836003867ea68bfe67fbfcb46028f261b4970cfd
Dan Shima <danning.cc@gmail.com>
2021-05-08 22:42:01 +0200

    add cat image to list

--- app/src/main/java/com/danshima/woof/CatListItem.kt
+++ app/src/main/java/com/danshima/woof/CatListItem.kt
@@ -1,9 +1,7 @@
 package com.danshima.woof
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,18 +10,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CatListItem(cat: Cat) {
     Card(
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
         elevation = 4.dp,
         backgroundColor = Color.LightGray,
         shape = RoundedCornerShape(corner = CornerSize(16.dp))
     ) {
         Row() {
+            loadImage(cat = cat)
             Column(
                 modifier = Modifier
                     .padding(16.dp)
@@ -35,4 +39,16 @@ fun CatListItem(cat: Cat) {
             }
         }
     }
+}
+
+@Composable
+private fun loadImage(cat: Cat) {
+    Image(
+        painter = painterResource(id = cat.imageId), 
+        contentDescription = cat.name, 
+        contentScale = ContentScale.Crop, 
+        modifier = Modifier
+            .padding(8.dp)
+            .size(60.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(16.dp))))
 }
